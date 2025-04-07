95870dca6bde9bf93b9192db474007b283d36465
sangjun <sangjoon12345@gmail.com>
2025-04-07 13:09:33 +0900

    feat: 마지막 접속 url 리다이렉트

--- src/main/java/com/trippia/travel/auth/loginhandler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/trippia/travel/auth/loginhandler/OAuth2LoginSuccessHandler.java
@@ -5,16 +5,25 @@ import com.trippia.travel.domain.common.Role;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
@@ -29,6 +38,13 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
             response.sendRedirect(redirectUrl);
             return;
         }
-        response.sendRedirect("/");
+
+        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request,response);
+        if (savedRequest != null) {
+            String targetUrl = savedRequest.getRedirectUrl();
+            redirectStrategy.sendRedirect(request, response, targetUrl);
+        } else {
+            redirectStrategy.sendRedirect(request, response, "/");
+        }
     }
 }
