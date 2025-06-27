ec6bab2665027d2763b13b8d7c5caacdb781ddef
sj0320 <112684207+sj0320@users.noreply.github.com>
2025-06-27 17:12:18 +0900

    Update TestController.java

--- src/main/java/com/trippia/travel/controller/TestController.java
+++ src/main/java/com/trippia/travel/controller/TestController.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.trippia.travel.domain.diarypost.diary.cache.DiaryRankingCacheService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
