0b3774c995434c355817bf47332a20e85b20ce6c
sangjun <sangjoon12345@gmail.com>
2025-07-04 13:29:47 +0900

    refactor: 서브쿼리를 통한 distinct 키워드 제거로 성능 개선

--- src/main/java/com/trippia/travel/controller/dto/post/request/CursorData.java
+++ src/main/java/com/trippia/travel/controller/dto/post/request/CursorData.java
@@ -3,11 +3,13 @@ package com.trippia.travel.controller.dto.post.request;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@ToString
 public class CursorData {
 
     private Long lastId;
